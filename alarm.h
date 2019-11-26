94dca379c65ddd11a4aecba6281ca09275f96bfa
maazasim-ara <58209878+maazasim-ara@users.noreply.github.com>
2019-11-26 22:57:11 +0500

    Update alarm.h

--- alarm.h
+++ alarm.h
@@ -3,6 +3,7 @@ int alarm(float lng, float lat){
 	FILE *alm;
 	wiringPiSetupGPIO();
 	const int INPUT_PIN = 5;
+	char buf[1000];
 	pinMode(INPUT_PIN, INPUT);
 
     while(true){
@@ -26,6 +27,9 @@ int alarm(float lng, float lat){
 			fprintf(alm, "\n-------\nAt long: %f lat: %f\nThere was an emergency was detected\nAn audio file is being saved has been", lng, lat);
 			fclose(alm);
 			system("cd /home/hp/Desktop/Shared && arecord -d 10 -f cd -r 44100 -c2 -t wav | lame -S -x -h -b 128 - `date +%Y%m%d%H%M`.wav");
+			snprintf(buf, sizeof(buf), "chromium http://www.google.com/maps/place/%s,%s",lng,lat);
+			system(buf)
+
 		}
     return 0;
 	}
