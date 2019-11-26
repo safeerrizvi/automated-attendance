92ee4b9f151f1049dafc84e0e0f74dd3b57c72b7
maazasim-ara <58209878+maazasim-ara@users.noreply.github.com>
2019-11-26 18:44:24 +0500

    Add files via upload
    safeer's module that has alarm function.

---
+++ alarm.h
@@ -0,0 +1,32 @@
+int alarm(float lng, float lat){
+
+	FILE *alm;
+	wiringPiSetupGPIO();
+	const int INPUT_PIN = 5;
+	pinMode(INPUT_PIN, INPUT);
+
+    while(true){
+	//Detects High Voltage on GPIO5, creates a txt file with  invokes shell command 'arecord'
+	if(digitalRead((INPUT_PIN) == HIGH)
+		{
+			//if statement to check weather the file contents are not null run everything below USE r+:
+			if(alm == NULL){
+			alm = fopen("/home/hp/Desktop/Shared/emergency log.txt", "w+");
+				printf("error opening file");
+				return 1;
+				}
+			fprintf(alm, "At long: %f lat: %f\nThere was an emergency was detected\nAn audio file is being saved has been", lng, lat);
+			fclose(alm);
+			system("cd /home/hp/Desktop/Shared && arecord -d 10 -f cd -r 44100 -c2 -t wav | lame -S -x -h -b 128 - `date +%Y%m%d%H%M`.wav");
+
+			//else if file read is not null file 
+			else(alm != NULL){
+			alm = fopen("/home/hp/Desktop/Shared/emergency log.txt", "a")
+				}
+			fprintf(alm, "\n-------\nAt long: %f lat: %f\nThere was an emergency was detected\nAn audio file is being saved has been", lng, lat);
+			fclose(alm);
+			system("cd /home/hp/Desktop/Shared && arecord -d 10 -f cd -r 44100 -c2 -t wav | lame -S -x -h -b 128 - `date +%Y%m%d%H%M`.wav");
+		}
+    return 0;
+	}
+}
