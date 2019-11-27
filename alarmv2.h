0a54e0e2ebf0348435634cf5be2078055d034faa
maazasim-ara <58209878+maazasim-ara@users.noreply.github.com>
2019-11-27 12:27:47 +0500

    Update alarmv2.h

--- alarmv2.h
+++ alarmv2.h
@@ -10,13 +10,13 @@ int alarm(float lng, float lat){
 		{
 			//if statement to check weather the file contents are not null run everything below USE r+:
 			if(alm == NULL){
-			alm = fopen("/home/hp/Desktop/Shared/emergency log.txt", "w+");
+			alm = fopen("/home/pi/Desktop/emergency log.txt", "w+");
 				printf("error opening file");
 				return 1;
 				}
 			fprintf(alm, "At long: %f lat: %f\nThere was an emergency was detected\nAn audio file is being saved has been", lng, lat);
 			fclose(alm);
-			system("cd /home/hp/Desktop/Shared && arecord -d 10 -f cd -r 44100 -c2 -t wav | lame -S -x -h -b 128 - `date +%Y%m%d%H%M`.wav");
+			system("cd /home/pi/Desktop/ && arecord -d 10 -f cd -r 44100 -c2 -t wav | lame -S -x -h -b 128 - `date +%Y%m%d%H%M`.wav");
 
 			//else if file read is not null file 
 			else(alm != NULL){
