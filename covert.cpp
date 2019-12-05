13b7a9345a79595e4d80911711fd53f70cea2768
maazasim-ara <58209878+maazasim-ara@users.noreply.github.com>
2019-12-06 00:01:41 +0500

    Update covert.cpp

--- covert.cpp
+++ covert.cpp
@@ -1,31 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-int main ()
-{
-    double longitude;
-    double latitude;
-    char buf[] ="4.44141345/qwe/6.7";
+
+Void parse (buf)
     char *pend;
     int i = 0;
-    char *p = strtok (buf, "/");
-    char *array[3];
+    char *p = strtok (buf, ":,");
+    char *array[100];
     
     while (p != NULL)
     {
         array[i++] = p;
-        p = strtok (NULL, "/");
+        p = strtok (NULL, ":,");
     }
     
-        printf("%s\n", array[0]);
+    printf("%s\n", array[4]
     printf("%s\n", array[2]);
-    double f1 = strtod(array[0], &pend);
-    double f2 = strtod(array[0], &pend);
+    double f1 = strtod(array[2], &pend);
+    double f2 = strtod(array[4], &pend);
     longitude=f1;
     latitude=f2;
-    printf("%.15F",longitude);
-    printf
+    printf("longitude:%.15F",longitude);
+    printf("latitude:%.15F",latitude);
     
     return 0;
 }
